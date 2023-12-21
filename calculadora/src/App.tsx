1ddd41a52f4aa904eda88a1c5530821218bda3f5
Claudio <junior040712@gmail.com>
2023-12-21 19:19:08 -0300

    Calculadora Finalizada

--- calculadora/src/App.tsx
+++ calculadora/src/App.tsx
@@ -1,34 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, FormEvent } from 'react'
+import logoImg from '../src/assets/logo.png'
 import './App.css'
 
+interface infoProps {
+  title: string;
+  rGasolina: string | number;
+  rAlcool: string | number;
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [gasolina, setGasolina] = useState(0);
+  const [alcool, setAlcool] = useState(0);
+  const [info, setInfo] = useState<infoProps>();
+
+  function calcular(event: FormEvent) {
+    event.preventDefault()
+
+    let calculo = (alcool / gasolina);
+    if (calculo <= 0.7) {
+      setInfo({
+        title: "Compensa usar Alcool",
+        rGasolina: formatarMoeda(gasolina),
+        rAlcool: formatarMoeda(alcool)
+      })
+    } else {
+      setInfo({
+        title: "Compensa usar Gasolina",
+        rGasolina: formatarMoeda(gasolina),
+        rAlcool: formatarMoeda(alcool)
+      })
+    }
+  }
+
+  function formatarMoeda(valor: number){
+    let valorFormatado = valor.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL"
+    })
+
+    return valorFormatado;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <main className='container'>
+        <img src={logoImg} alt="Logo Calculadora" className='logo' />
+        <h1 className='title'>Qual a melhor opção?</h1>
+        <form className='form' onSubmit={calcular}>
+          <label>Alcool (Preço por litro)</label>
+          <input
+            type="number"
+            className='input'
+            value={alcool}
+            onChange={(e) => setAlcool(Number(e.target.value))}
+            placeholder='4,90'
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <label>Gasolina (Preço por litro)</label>
+          <input
+            type="number"
+            className='input'
+            value={gasolina}
+            onChange={(e) => setGasolina(Number(e.target.value))}
+            placeholder='4,90'
+            min="1"
+            step="0.01"
+            required
+          />
+
+          <input type="submit" value="Calcular" className='buttom' />
+        </form>
+
+        {info && Object.keys(info).length > 0 && (
+          <section className='result'>
+            <h2 className='result-title'>{info.title}</h2>
+            <span>Alcool {info.rAlcool}</span>
+            <span>Gasolina {info.rGasolina}</span>
+          </section>
+        )}
+
+      </main>
+    </div>
   )
 }
 
